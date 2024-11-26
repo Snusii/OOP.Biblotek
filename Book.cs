dec9c73f340f17fedb5684929b5aa859ab794f51
Snusii <Snusii1995@gmail.com>
2024-11-26 23:58:31 +0100

    Added author to book.cs

---
+++ Book.cs
@@ -0,0 +1,13 @@
+using System.Dynamic;
+
+class Book {
+    public string title;
+    public string author;
+
+    public Book(string title, string author) {
+
+        this.title = title;
+        this.author = author;
+    }
+
+}
